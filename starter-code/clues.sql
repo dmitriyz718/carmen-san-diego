932aa43bd474888cadb2466abce2de85b1c926ee
u101004132 <dzag17188@gmail.com>
2020-08-10 19:53:59 -0400

    clue 2

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -11,7 +11,9 @@ LIMIT 1;
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+SELECT *
+FROM countrylanguage
+WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
