dc2b32d0415f7734516ca98044ac1206c48d53fc
u101004132 <dzag17188@gmail.com>
2020-08-10 19:55:36 -0400

    clue 4

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -30,10 +30,13 @@ WHERE language = 'Italian'
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
- -- There are only two cities she could be flying to in the country. One is named the same as the country – that
- -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
- --  be flying to.
-
+-- There are only two cities she could be flying to in the country. One is named the same as the country – that
+-- would be too obvious. We're following our gut on this one; find out what other city in that country she might
+--  be flying to.
+SELECT *
+FROM city
+WHERE countrycode = 'SMR'
+    AND name != 'San Marino';
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
